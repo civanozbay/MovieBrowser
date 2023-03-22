8915331832f21855b306b954234ea451e1775402
civanozbay <civan417@hotmail.com>
2023-03-22 10:00:39 +0100

    making object reusable

--- index.js
+++ index.js
@@ -1,35 +1,90 @@
-//3a4f6c19
-//http://www.omdbapi.com/?apikey=3a4f6c19&
-
-const fetchData = async (searchTerm) => {
-    const response = await axios.get('http://www.omdbapi.com/',{
-        params: { // this object turn into string and add end of the url. One of the advantages of axios library.
-            apikey : '3a4f6c19',
-            s : searchTerm
-            // i : 'tt0848228'
-        }
+const fetchData = async searchTerm => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: 'd9835cc5',
+        s: searchTerm
+      }
     });
-    if(response.data.Error){
-        return [];
+  
+    if (response.data.Error) {
+      return [];
     }
-
+  
     return response.data.Search;
-}
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-  <label><b>Search For a Movie</b></label>
-  <input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
+  };
+  
+  createAutoComplete({
+    root: document.querySelector('.autocomplete'),
+    renderOption(movie){
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+        return `
+            <img src="${imgSrc}" />
+            ${movie.Title} (${movie.Year})
+        `;
+    },
+    onOptionSelect(movie) {
+      onMovieSelect(movie);
+    },
+    inputValue(movie){
+        return movie.Title;
+    }
+  });
 
 
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
+  
+  const onMovieSelect = async movie => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: 'd9835cc5',
+        i: movie.imdbID
+      }
+    });
+  
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+  };
+  
+  const movieTemplate = movieDetail => {
+    return `
+      <article class="media">
+        <figure class="media-left">
+          <p class="image">
+            <img src="${movieDetail.Poster}" />
+          </p>
+        </figure>
+        <div class="media-content">
+          <div class="content">
+            <h1>${movieDetail.Title}</h1>
+            <h4>${movieDetail.Genre}</h4>
+            <p>${movieDetail.Plot}</p>
+          </div>
+        </div>
+      </article>
+      <article class="notification is-primary">
+        <p class="title">${movieDetail.Awards}</p>
+        <p class="subtitle">Awards</p>
+      </article>
+      <article class="notification is-primary">
+        <p class="title">${movieDetail.BoxOffice}</p>
+        <p class="subtitle">Box Office</p>
+      </article>
+      <article class="notification is-primary">
+        <p class="title">${movieDetail.Metascore}</p>
+        <p class="subtitle">Metascore</p>
+      </article>
+      <article class="notification is-primary">
+        <p class="title">${movieDetail.imdbRating}</p>
+        <p class="subtitle">IMDB Rating</p>
+      </article>
+      <article class="notification is-primary">
+        <p class="title">${movieDetail.imdbVotes}</p>
+        <p class="subtitle">IMDB Votes</p>
+      </article>
+    `;
+  };
+  
+
+
+
 
 
 
@@ -43,65 +98,3 @@ const resultsWrapper = document.querySelector('.results');
 //         fetchData(event.target.value)},1000)
 // }
 // input.addEventListener('input',onInput)
-
-const onInput =  async event => {
-    // define 
-    const movies = await fetchData(event.target.value); // we can get access to value through target.value 
-    if(!movies.length){
-        dropdown.classList.remove('is-active')
-    }
-
-    resultsWrapper.innerHTML = '';
-    dropdown.classList.add('is-active'); // we are opening to dropdown with adding to classlist
-    for (let movie of movies) {
-        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-        const div = document.createElement('a');
-        
-        div.classList.add('dropdown-item'); 
-        div.innerHTML = `
-          <img src="${imgSrc}" />
-          ${movie.Title}
-        `;
-        div.addEventListener('click',() => { 
-            dropdown.classList.remove('is-active');
-            input.value=movie.Title;
-            onMovieSelect(movie);
-        })
-        resultsWrapper.appendChild(div);
-    }
-}
-input.addEventListener('input',debounce(onInput,1000));
-
-document.addEventListener('click',event => {
-    if(!root.contains(event.target)){
-        dropdown.classList.remove('is-active')
-    }
-})
-const onMovieSelect = async movie => {
-    const response = await axios.get('http://www.omdbapi.com/',{
-        params: { // this object turn into string and add end of the url. One of the advantages of axios library.
-            apikey : '3a4f6c19',
-            i : movie.imdbID
-        }
-    })
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
-};
-
-const movieTemplate = (movieDetail) => {
-    return `
-        <article class="media">
-            <figure class="media-left">
-                <p class="image">
-                    <img src="${movieDetail.Poster}"/>
-                </p>
-            </figure>
-            <div class="media-content">
-                <div class="content">
-                    <h1>${movieDetail.Title}</h1>
-                    <h4>${movieDetail.Genre}</h4>
-                    <p>${movieDetail.Plot}</p>
-                </div>
-            </div>
-        </article>
-    `
-}
